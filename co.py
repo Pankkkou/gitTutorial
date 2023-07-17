04e1ec4a4103cbca48f4ac65a4eaa68deda64bcc
Proton <126896728+9712w@users.noreply.github.com>
2023-07-17 17:25:49 +0300

    Add co.py

---
+++ co.py
@@ -0,0 +1,31 @@
+x = 3
+
+if x > 3:
+	print("x > 3")
+elif x < 3:
+	print("x < 3")
+else:
+	print("x == 3")
+
+
+dayTime = input("What is the time right now? ")
+
+if dayTime.lower() == "afternoon":
+    print("Now it is afternoon.")
+else:
+    print("It is not afternoon.")
+
+
+user_input = input("input something. ")
+if user_input == "helloy":
+    print("helloy")
+else:
+    print("wrong text")
+   
+x = 10
+
+if x % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
